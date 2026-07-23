f40a9a128ef74063ed380453b528de264f440330
LucasDuarteV <lucasdv2733@gmail.com>
2026-07-23 11:53:17 -0300

    Conectando ao banco de dados

--- job-finder/app.js
+++ job-finder/app.js
@@ -1,12 +1,23 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const db = require('./db/connection.js');
 
-const PORT = 3000
+const PORT = 3000;
 
-app.get('/', (req,res) => {
-    res.send('Rodando sucesso')
-})
+// conexão com banco
+db.authenticate()
+    .then(() => {
+        console.log('Conectou ao banco com sucesso!');
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta: ${PORT}`)
-})
+        app.listen(PORT, () => {
+            console.log(`Servidor rodando na porta: ${PORT}`);
+        });
+    })
+    .catch((erro) => {
+        console.log('Erro ao conectar com o banco:', erro);
+    });
+
+// rotas
+app.get('/', (req, res) => {
+    res.send('Rodando com sucesso');
+});
